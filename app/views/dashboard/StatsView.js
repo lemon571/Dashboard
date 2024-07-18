dda30d02bcc556694050cf9deebc32d707ded6f3
lemon571 <ya.bryzgalova@gmail.com>
2024-07-18 09:03:18 +0200

    Working files without button

--- app/views/dashboard/StatsView.js
+++ app/views/dashboard/StatsView.js
@@ -1,9 +1,6 @@
 define(['jquery', 'underscore', 'backbone','app',
     'models/app_config', // api/status/uptime
     'models/user_config', // api/settings/user-preference
-    //'views/dashboard/QuickButtonViewPC',
-    //'views/dashboard/QuickButtonViewHVM',
-    'views.dashboard/QuickButtonView3',
     'views/dashboard/SystemStatusView',
     'views/dashboard/ProductInformationView',
     'views/dashboard/NetworkInformationView',
@@ -12,9 +9,7 @@ define(['jquery', 'underscore', 'backbone','app',
     'text!templates/dashboard/stats.html',
     'i18n!:stats'
 ],
-function($, _, Backbone, app ,AppConfigModel, UserConfigModel, 
-    //QuickButtonViewPC, QuickButtonViewHVM, 
-    QuickButtonView3, SystemStatusView, ProductInformationView, NetworkInformationView, FirmwareInformationView, StatsTemplate, CommonStrings) {
+function($, _, Backbone, app ,AppConfigModel, UserConfigModel, SystemStatusView, ProductInformationView, NetworkInformationView, FirmwareInformationView, StatsTemplate, CommonStrings) {
 
     var view = Backbone.View.extend({
 
@@ -36,15 +31,7 @@ function($, _, Backbone, app ,AppConfigModel, UserConfigModel,
             });
 
             this.acm.trigger("change", this.acm, this.acm.get('minutes_per_count'));
-            
-            var quickbutton = new QuickButtonView3()
-            this.inserView("#quick-button", quickbutton).render();
-            /*var quickbutton1 = new QuickButtonViewPC();
-            this.insertView("#pc-button", quickbutton1).render();
-
-            var quickbutton2 = new QuickButtonViewHVM();
-            this.insertView("#hvm-button", quickbutton2).render();
-            */
+    
             var stat = new SystemStatusView();
             this.insertView("#system-status-info", stat).render();
 
